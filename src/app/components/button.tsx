1c58b9bcc4ca85b95bbf1032ad93047c9030c576
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2024-08-02 01:49:47 -0300

    chore: Update Button and Logo components, improve styling and accessibility

--- src/app/components/button.tsx
+++ src/app/components/button.tsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React from "react";
 
 type ButtonProps = {
   children: React.ReactNode;
   className?: string;
-}
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-export default function Button({children, className, ...props}: ButtonProps) {
+export default function Button({ children, className, ...props }: ButtonProps) {
   return (
-    <button type="button" className='w-full bg-navy text-accent text-md py-2 rounded-sm hover:opacity-90' {...props}>{children}</button>
-  )
+    <button
+      type="button"
+      className="w-full bg-navy text-accent text-md font-semibold py-2 rounded-md hover:opacity-90"
+      {...props}
+    >
+      {children}
+    </button>
+  );
 }
--- src/app/components/logo.tsx
+++ src/app/components/logo.tsx
@@ -1,12 +1,13 @@
-import Image from "next/image";
 import React from "react";
+import { IoTimer } from "react-icons/io5";
 
 export default function Logo() {
   return (
-    <div className="inline-flex items-center">
-      <div className="inline-flex font-bold text-secondary text-4xl">
+    <div className="inline-flex items-center justify-center text-mint w-full">
+      <IoTimer className="w-8 h-8" />
+      <div className="inline-flex font-bold text-4xl">
       <p>Habitos.</p>
-      <p className=" text-mint">diários</p>
+      <p className=" text-secondary">diários</p>
       </div>
     </div>
   );
