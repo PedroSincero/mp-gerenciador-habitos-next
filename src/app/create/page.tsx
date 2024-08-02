ead4e5844bebca333859208289e58bae56863250
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2024-08-01 23:41:51 -0300

    chore: Update project configuration files and dependencies

---
+++ src/app/create/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function HabitCreate() {
+  return (
+    <div>Teste</div>
+  )
+}
---
+++ src/app/details/[id]/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function HabitDetails() {
+  return (
+    <div>HabitDetails</div>
+  )
+}
